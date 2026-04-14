d2db5036db1c03db863e69c90f26b2adb29e3c1c
SuxinnewOvO <zhang105680312@163.com>
2026-04-14 23:16:27 +0800

    feat(sensor): 完成DHT12温湿度采集功能，稳定输出版本

--- MDK-ARM/Project/main.d
+++ MDK-ARM/Project/main.d
@@ -23,4 +23,4 @@ project/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_pwr.h \
   ..\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal_uart.h \
   ..\Core\Inc\adc.h ..\Core\Inc\dma.h ..\Core\Inc\i2c.h \
-  ..\Core\Inc\usart.h ..\Core\Inc\gpio.h
+  ..\Core\Inc\usart.h ..\Core\Inc\gpio.h ..\Core\Inc\dth12.h
